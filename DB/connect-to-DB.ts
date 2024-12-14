f5680928a6321980ffb6a40a13128a7f59d02d1f
mohamad <mohamadshiravi85@gmail.com>
2024-12-14 15:59:59 +0330

    add database

--- DB/connect-to-DB.ts
+++ DB/connect-to-DB.ts
@@ -5,6 +5,6 @@ export default async function ConnectTODb() {
     if (mongoose.connections[0].readyState) {
       return false;
     }
-    await mongoose.connect("mongodb://localhost:27017/cms-panel");
+    await mongoose.connect(process.env.MONGODB_URI!);
   } catch (error) {}
 }
